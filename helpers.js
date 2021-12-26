3bf793312e34e160198aa755fa870b75d6b189c4
Siarhei Kvachonak <s.kvachenok@syberry.com>
2021-12-26 20:24:54 +0100

    Create push server

---
+++ helpers.js
@@ -0,0 +1,60 @@
+const fsp = require('fs').promises;
+const webpush = require('web-push');
+
+const getSubscriptionsFromFile = async () => {
+  try {
+    const data = await fsp.readFile('subscriptions.json', 'utf8');
+    const existingSubscriptions = data ? JSON.parse(data) : {};
+    return existingSubscriptions;
+  } catch (err) {
+    console.log('READ SUBSCRIPTIONS FILE ERROR', err);
+    return null;
+  }
+};
+
+const appendSubscription = async subscription => {
+  if (!subscription) return;
+
+  try {
+    const { endpoint } = subscription;
+    let endpointKey = endpoint.split('://')[1];
+    endpointKey = endpointKey.split('/')[0];
+
+    const existingSubscriptions = await getSubscriptionsFromFile();
+
+    const result = {
+      ...existingSubscriptions,
+      [endpointKey]: subscription,
+    };
+
+    const formattedJsonResult = JSON.stringify(result, null, 2);
+    const resultData = new Uint8Array(Buffer.from(formattedJsonResult));
+
+    await fsp.writeFile('subscriptions.json', resultData);
+  } catch (err) {
+    console.log('WRITE FILE ERROR', err);
+  }
+};
+
+const sendNotification = async payload => {
+  webpush.setVapidDetails(`mailto:${process.env.EMAIL}`, process.env.VAPID_PUBLIC_KEY, process.env.VAPID_PRIVATE_KEY);
+
+  const subscriptions = await getSubscriptionsFromFile();
+  const subscriptionList = Object.values(subscriptions);
+
+  for (const subscription of subscriptionList) {
+    const { endpoint, keys } = subscription;
+
+    const pushSubscription = {
+      endpoint,
+      keys,
+    };
+
+    await webpush.sendNotification(pushSubscription, payload);
+  }
+};
+
+module.exports = {
+  appendSubscription,
+  sendNotification,
+};
---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { appendSubscription, sendNotification } = require('./helpers');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+const jsonParser = bodyParser.json();
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'x-access-token, Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.post('/basket/purchase', async function (req, res) {
+  console.log('PURCHASE');
+  try {
+    const payload = JSON.stringify({
+      title: 'Successfully paid',
+      content: 'The time slots succeefully provided',
+      data: {
+        openUrl: '/',
+      },
+    });
+
+    await sendNotification(payload);
+    res.json(payload);
+  } catch (err) {
+    console.log('SEND NOTIFICATION ERROR', err);
+  }
+});
+
+app.post('/app/notify', jsonParser, async function (req, res) {
+  console.log('NOTIFY');
+  console.log('body', req.body);
+  try {
+    const { title, content, data } = req.body || {};
+
+    const payload = JSON.stringify({
+      title,
+      content,
+      data: data || null,
+    });
+
+    await sendNotification(payload);
+    res.json(payload);
+  } catch (err) {
+    console.log('SEND NOTIFICATION ERROR', err);
+  }
+});
+
+app.post('/app/register', jsonParser, async function (req, res) {
+  try {
+    await appendSubscription(req.body);
+    res.json({});
+  } catch (err) {
+    console.log('REGISTER SUBSCRIPTION ERROR', err);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
+});
